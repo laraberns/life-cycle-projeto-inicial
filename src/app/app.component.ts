4f8145746a34f59dd7ea9ad23ccdb3913a238b40
Lara Pereira <larabernspereira@gmail.com>
2025-10-07 17:21:43 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'app-lista-de-compras';
+  listaDeCompras!: Array<Item>;
+  itemParaSerEditado!: Item;
+
+  constructor(private listaService: ListaDeCompraService) {}
+
+  ngOnInit(): void {
+    this.listaDeCompras = this.listaService.getListaDeCompra();
+    console.log(this.listaDeCompras);
+  }
+
+  editarItem(item: Item) {
+    this.itemParaSerEditado = item;
+  }
+}
---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css'],
+})
+export class ItemComponent implements OnInit, OnChanges {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+
+  faPen = faPen;
+  faTrash = faTrash;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
+
+  ngOnChanges() {
+    console.log('OnChanges');
+  }
+
+  editarItem() {
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+}
