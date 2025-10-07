2bf2a77fb583f2aacde0f2c9fed47e2e971b0151
Lara Pereira <larabernspereira@gmail.com>
2025-10-07 17:50:00 -0300

    add css style when item is checked and settle in local storage

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -26,5 +33,13 @@ export class ItemComponent implements OnInit, OnChanges {
 
   editarItem() {
     this.emitindoItemParaEditar.emit(this.item);
+  }
+
+  checarItem() {
+    if (this.item.comprado == true) {
+      this.item.comprado = false;
+    } else {
+      this.item.comprado = true;
+    }
   }
 }
